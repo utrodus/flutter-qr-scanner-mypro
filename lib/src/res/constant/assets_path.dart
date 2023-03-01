a4d096ead539c7906f5d96fbfe7106b2e526c272
utrodus <come.programmer@gmail.com>
2023-03-01 14:57:58 +0700

    feat: add new image assets and create contants assets_path - image assets for onboarding 1 & 2 - class for define constants assets path

---
+++ lib/src/res/constant/assets_path.dart
@@ -0,0 +1,7 @@
+/// Constant Asset Path Configuration for easy access & safe typing
+class AssetsPath {
+  /// icon & image path
+  static const String icLogo = "assets/img/ic_logo.png";
+  static const String imgOnBoarding1 = "assets/img/img_onboarding_1.png";
+  static const String imgOnBoarding2 = "assets/img/img_onboarding_2.png";
+}
